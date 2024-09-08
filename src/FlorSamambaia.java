4c08f0ddf793f76b0f2f1f0684c51b0d077dc95a
oligregz <gregorysak90@gmail.com>
2024-09-08 11:28:05 -0300

    [logic lests] - start tests :rocket:

---
+++ src/FlorSamambaia.java
@@ -0,0 +1,5 @@
+public class FlorSamambaia {
+    public static String tentativaDesenhar(int r1, int x1, int y1, int r2, int x2, int y2) {
+        return "ok";
+    }
+}
---
+++ src/Snail.java
@@ -0,0 +1,6 @@
+public class Snail {
+  public static void retornarExtremos(String[] args) {
+    System.out.println("Hello, World2!");
+
+  }
+}
